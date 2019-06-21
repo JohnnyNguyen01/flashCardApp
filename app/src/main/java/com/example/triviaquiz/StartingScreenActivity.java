c5c1e632a4cb593e45e2ba8f855349515e0e86be
Johnny <johnnynguyenwork@gmail.com>
2019-06-21 14:52:50 +1000

    initial commit

---
+++ app/src/main/java/com/example/triviaquiz/StartingScreenActivity.java
@@ -0,0 +1,13 @@
+package com.example.triviaquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StartingScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.starting_screen_activity);
+    }
+}

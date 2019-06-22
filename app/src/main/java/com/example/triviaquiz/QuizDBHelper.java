95aa662859ee18464f58b00f34cf5f2d00d4ae78
DESKTOP-V45K5BO\Johnny the sick cunt <johnnynguyenwork@gmail.com>
2019-06-22 13:23:24 +1000

    completed sqLite database

--- app/src/main/java/com/example/triviaquiz/QuizDBHelper.java
+++ app/src/main/java/com/example/triviaquiz/QuizDBHelper.java
@@ -2,13 +2,17 @@ package com.example.triviaquiz;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+
 import com.example.triviaquiz.QuizContract;
 
+import java.util.ArrayList;
+
 /*
-*  TODO: https://www.youtube.com/watch?v=pEDVdSUuWXE <----- next video
-* */
+ *  TODO: https://www.youtube.com/watch?v=pEDVdSUuWXE <----- next video
+ * */
 
 public class QuizDBHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "MyQuiz.db";
@@ -30,10 +34,10 @@ public class QuizDBHelper extends SQLiteOpenHelper {
                 QuizContract.QuestionsTable.TABLE_NAME + " ( " +
                 QuizContract.QuestionsTable._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
                 QuizContract.QuestionsTable.COLUMN_QUESTION + " TEXT, " +
-                QuizContract.QuestionsTable.COLUMN_OPTION1 + " TEXT, "+
-                QuizContract.QuestionsTable.COLUMN_OPTION2 + " TEXT, "+
-                QuizContract.QuestionsTable.COLUMN_OPTION3 + " TEXT, "+
-                QuizContract.QuestionsTable.COLUMN_OPTION4 + " TEXT, "+
+                QuizContract.QuestionsTable.COLUMN_OPTION1 + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_OPTION2 + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_OPTION3 + " TEXT, " +
+                QuizContract.QuestionsTable.COLUMN_OPTION4 + " TEXT, " +
                 QuizContract.QuestionsTable.COLUMN_ANSWER_NUMBER + " INTEGER" +
                 ")";
 
@@ -43,27 +47,31 @@ public class QuizDBHelper extends SQLiteOpenHelper {
 
     //only call onUpgrade if app is in production. s
     /* STEPS:
-    * define changes in SQL_CREATE_QUESTIONS_TABLE
-    * call db.execSQL("DROP TABLE IF EXISTS" + QuestionsTable.TABLE_NAME
-    * call onCreate(db)
-    * */
+     * define changes in SQL_CREATE_QUESTIONS_TABLE
+     * call db.execSQL("DROP TABLE IF EXISTS" + QuestionsTable.TABLE_NAME
+     * call onCreate(db)
+     * */
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
 
     /*Create questions initially*/
-    private void fillQuestionsTable(){
+    private void fillQuestionsTable() {
         //TODO: get rid of dummy data
-        Question q1 = new Question("A is correct", "A","B","C","D",1);
-        Question q2 = new Question("B is correct", "A","B","C","D",1);
-        Question q3 = new Question("C is correct", "A","B","C","D",1);
-        Question q4 = new Question("D is correct", "A","B","C","D",1);
+        Question q1 = new Question("A is correct", "A", "B", "C", "D", 1);
+        addQuestion(q1);
+        Question q2 = new Question("B is correct", "A", "B", "C", "D", 1);
+        addQuestion(q2);
+        Question q3 = new Question("C is correct", "A", "B", "C", "D", 1);
+        addQuestion(q3);
+        Question q4 = new Question("D is correct", "A", "B", "C", "D", 1);
+        addQuestion(q4);
 
     }
 
     //add new questions
-    private void addQuestion(Question question){
+    private void addQuestion(Question question) {
         ContentValues cv = new ContentValues();
         //param 1 = what table you want to enter valeus into, param 2 = question object field
         cv.put(QuizContract.QuestionsTable.COLUMN_QUESTION, question.getQuestion());
@@ -73,6 +81,30 @@ public class QuizDBHelper extends SQLiteOpenHelper {
         cv.put(QuizContract.QuestionsTable.COLUMN_OPTION4, question.getOption4());
         cv.put(QuizContract.QuestionsTable.COLUMN_ANSWER_NUMBER, question.getAnswerNr());
 
-        db.insert(QuizContract.QuestionsTable.TABLE_NAME,null,cv);
+        db.insert(QuizContract.QuestionsTable.TABLE_NAME, null, cv);
+    }
+
+    public ArrayList<Question> getAllQuestions() {
+        ArrayList<Question> questionsList = new ArrayList<>();
+        db = getReadableDatabase(); //reference to database to grab data out of. when method is called the first time, it calles onCreateDatabasee method
+        Cursor c = db.rawQuery("SELECT * FROM " + QuizContract.QuestionsTable.TABLE_NAME, null); //cursor allows queries
+        /*
+         * Queries first entry, then moves onto next entry if it exists
+         * */
+        if (c.moveToFirst()) {
+            do {
+                Question question = new Question();
+                question.setQuestion(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_QUESTION)));
+                question.setOption1(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION1)));
+                question.setOption2(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION2)));
+                question.setOption3(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION3)));
+                question.setOption4(c.getString(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_OPTION4)));
+                question.setAnswerNr(c.getInt(c.getColumnIndex(QuizContract.QuestionsTable.COLUMN_ANSWER_NUMBER)));
+                questionsList.add(question);
+            } while (c.moveToNext());
+        }
+
+        c.close();
+        return questionsList;
     }
 }
--- app/src/main/java/com/example/triviaquiz/Quiz_Activity.java
+++ app/src/main/java/com/example/triviaquiz/Quiz_Activity.java
@@ -2,12 +2,43 @@ package com.example.triviaquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Quiz_Activity extends AppCompatActivity {
+    private TextView questionTV;
+    private TextView countDownTV;
+    private TextView scoreTV;
+    private TextView questionCountTV;
+    private Button confirmBtn;
+    private RadioButton option1;
+    private RadioButton option2;
+    private RadioButton option3;
+    private RadioButton option4;
+    private ArrayList<Question> questionsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_);
+        initialize();
+    }
+
+    private void initialize(){
+        questionTV = findViewById(R.id.questionTV);
+        countDownTV = findViewById(R.id.countDownTV);
+        scoreTV = findViewById(R.id.scoreTV);
+        questionCountTV = findViewById(R.id.questionCountTV);
+        confirmBtn = findViewById(R.id.confirmBtn);
+        option1 = findViewById(R.id.option1);
+        option2 = findViewById(R.id.option2);
+        option3 = findViewById(R.id.option3);
+        option4 = findViewById(R.id.option4);
+
+        QuizDBHelper dbHelper = new QuizDBHelper(getApplicationContext());
+        questionsList = dbHelper.getAllQuestions();
     }
 }

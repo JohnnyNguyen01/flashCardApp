db21336e52f032b56cb40c5d08872436481cd2fc
DESKTOP-V45K5BO\Johnny the sick cunt <johnnynguyenwork@gmail.com>
2019-06-22 14:30:30 +1000

    Added functions to Quiz_Activity

--- app/src/main/java/com/example/triviaquiz/Quiz_Activity.java
+++ app/src/main/java/com/example/triviaquiz/Quiz_Activity.java
@@ -1,5 +1,6 @@
 package com.example.triviaquiz;
 
+import android.content.res.ColorStateList;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -7,6 +8,7 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /*
 * TODO: https://www.youtube.com/watch?v=tlgrX3HF6AI <-- next video
@@ -18,11 +20,20 @@ public class Quiz_Activity extends AppCompatActivity {
     private TextView scoreTV;
     private TextView questionCountTV;
     private Button confirmBtn;
-    private RadioButton option1;
-    private RadioButton option2;
-    private RadioButton option3;
-    private RadioButton option4;
+    private RadioButton option1RB;
+    private RadioButton option2RB;
+    private RadioButton option3RB;
+    private RadioButton option4RB;
+
+    private ColorStateList textColourDefaultRB; //save default RB text color to change later according to correct answer
+
     private ArrayList<Question> questionsList;
+
+    private int questionCounter;
+    private int questionTotalCount;
+    private Question currentQuestion;
+    private int score;
+    private boolean answered;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,12 +48,46 @@ public class Quiz_Activity extends AppCompatActivity {
         scoreTV = findViewById(R.id.scoreTV);
         questionCountTV = findViewById(R.id.questionCountTV);
         confirmBtn = findViewById(R.id.confirmBtn);
-        option1 = findViewById(R.id.option1);
-        option2 = findViewById(R.id.option2);
-        option3 = findViewById(R.id.option3);
-        option4 = findViewById(R.id.option4);
+        option1RB = findViewById(R.id.option1);
+        option2RB = findViewById(R.id.option2);
+        option3RB = findViewById(R.id.option3);
+        option4RB = findViewById(R.id.option4);
+
+        textColourDefaultRB = option1RB.getTextColors();
 
         QuizDBHelper dbHelper = new QuizDBHelper(getApplicationContext());
         questionsList = dbHelper.getAllQuestions();
+        questionTotalCount = questionsList.size();
+        Collections.shuffle(questionsList); // randomizes appearance of questions
+        showNextQuestion();
+    }
+
+    private void showNextQuestion(){
+        option1RB.setTextColor(textColourDefaultRB);
+        option2RB.setTextColor(textColourDefaultRB);
+        option3RB.setTextColor(textColourDefaultRB);
+        option4RB.setTextColor(textColourDefaultRB);
+
+        if(questionCounter < questionTotalCount){
+            currentQuestion = questionsList.get(questionCounter);
+
+            questionTV.setText(currentQuestion.getQuestion());
+            option1RB.setText(currentQuestion.getOption1());
+            option2RB.setText(currentQuestion.getOption2());
+            option3RB.setText(currentQuestion.getOption3());
+            option4RB.setText(currentQuestion.getOption4());
+
+            questionCounter++;
+            questionCountTV.setText("Question: " + questionCounter + " / " + questionTotalCount);
+            answered = false;
+            confirmBtn.setText("CONFIRM");
+        }else{
+            finishQuiz();
+        }
+
+    }
+
+    private void finishQuiz(){
+        finish();
     }
 }
